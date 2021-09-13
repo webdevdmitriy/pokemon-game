b51ea6a4d59163a0cc86f8424ba6350e821e478b
Plotnikov Dmitriy <36085444+webdevdmitriy@users.noreply.github.com>
2021-09-13 21:47:23 +0300

    Созданые новые компоненты

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,15 @@
+import s from './style.module.css'
+const NavBar = () => {
+	return (
+		<nav class='root'>
+			<div class='navWrapper'>
+				<p class='brand'>LOGO</p>
+				<a class='menuButton active'>
+					<span />
+				</a>
+			</div>
+		</nav>
+	)
+}
+
+export default NavBar
