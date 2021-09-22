f0263ab69ec5e35e55eae79b02d8d62b643d29c4
Plotnikov Dmitriy <36085444+webdevdmitriy@users.noreply.github.com>
2021-09-22 19:24:36 +0300

    Created Board

--- src/routes/Game/routes/Board/index.js
+++ src/routes/Game/routes/Board/index.js
@@ -1,10 +1,27 @@
 import { useContext } from 'react'
+import { useHistory } from 'react-router'
+import { useEffect } from 'react/cjs/react.development'
+
 import PokemonCard from '../../../../components/PokemonCard'
 import { PokemonContext } from '../../../../context/PokemonContext'
 import s from './style.module.css'
 
 const BoardPage = () => {
+	const [board, setBoard] = useState([])
 	const { pokemon } = useContext(PokemonContext)
+	const history = useHistory()
+
+	// if (Object.keys(pokemons).length === 0) {
+	// 	history.replace('/game')
+	// }
+	useEffect(async () => {
+		const boardResponce = await fetch('https://reactmarathon-api.netlify.app/api/board')
+		const boardRequest = await boardResponce.json()
+
+		setBoard(boardRequest.data)
+	}, [])
+
+	const handleClickBoardPlate = position => {}
 	return (
 		<div className={s.root}>
 			<div className={s.playerOne}>
@@ -22,15 +39,15 @@ const BoardPage = () => {
 				))}
 			</div>
 			<div className={s.board}>
-				<div className={s.boardPlate}>1</div>
-				<div className={s.boardPlate}>2</div>
-				<div className={s.boardPlate}>3</div>
-				<div className={s.boardPlate}>4</div>
-				<div className={s.boardPlate}>5</div>
-				<div className={s.boardPlate}>6</div>
-				<div className={s.boardPlate}>7</div>
-				<div className={s.boardPlate}>8</div>
-				<div className={s.boardPlate}>9</div>
+				{board.map(item => (
+					<div
+						key={item.position}
+						className={s.boardPlate}
+						onClick={() => !item.card && handleClickBoardPlate(item.position)}
+					>
+						{item.card && <PokemonCard {...item} minimize />}
+					</div>
+				))}
 			</div>
 		</div>
 	)
