29af061aaecc0a29dc31b96739bec438a4c7d65c
Dmitriy <plotnikovdi95@gmail.com>
2021-09-23 16:43:47 +0300

    Доработали

---
+++ src/routes/Game/routes/Board/component/PlayerBoard/index.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+import PokemonCard from '../../../../../../components/PokemonCard'
+import cn from 'classnames'
+
+import s from './style.module.css'
+
+const PlayerBoard = ({ cards, onClickCard }) => {
+	const [isSelected, setSelected] = useState(null)
+	return (
+		<>
+			{cards.map(item => (
+				<div
+					className={cn(s.cardBoard, { [s.selected]: isSelected === item.id })}
+					onClick={() => {
+						setSelected(item.id)
+						onClickCard && onClickCard(item)
+					}}
+				>
+					<PokemonCard
+						key={item.id}
+						name={item.name}
+						img={item.img}
+						id={item.id}
+						type={item.type}
+						values={item.values}
+						isActive={true}
+					/>
+				</div>
+			))}
+		</>
+	)
+}
+
+export default PlayerBoard
--- src/routes/Game/routes/Board/index.js
+++ src/routes/Game/routes/Board/index.js
@@ -1,42 +1,42 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { useHistory } from 'react-router'
 import { useEffect } from 'react/cjs/react.development'
 
 import PokemonCard from '../../../../components/PokemonCard'
 import { PokemonContext } from '../../../../context/PokemonContext'
+import PlayerBoard from './component/PlayerBoard'
 import s from './style.module.css'
 
 const BoardPage = () => {
 	const [board, setBoard] = useState([])
+	const [Player2, setPlayer2] = useState([])
+	const [choiceCard, setChoiceCard] = useState(null)
+
 	const { pokemon } = useContext(PokemonContext)
 	const history = useHistory()
 
 	// if (Object.keys(pokemons).length === 0) {
 	// 	history.replace('/game')
 	// }
+
+	console.log(Player2)
 	useEffect(async () => {
 		const boardResponce = await fetch('https://reactmarathon-api.netlify.app/api/board')
 		const boardRequest = await boardResponce.json()
 
 		setBoard(boardRequest.data)
+
+		const Player2Responce = await fetch('https://reactmarathon-api.netlify.app/api/create-player')
+		const Player2Request = await Player2Responce.json()
+
+		setPlayer2(Player2Request.data)
 	}, [])
 
 	const handleClickBoardPlate = position => {}
 	return (
 		<div className={s.root}>
 			<div className={s.playerOne}>
-				{Object.values(pokemon).map(({ id, name, img, type, values }) => (
-					<PokemonCard
-						className={s.card}
-						key={id}
-						name={name}
-						img={img}
-						id={id}
-						type={type}
-						values={values}
-						isActive={true}
-					/>
-				))}
+				{<PlayerBoard cards={Object.values(pokemon)} onClickCard={card => setChoiceCard(card)} />}
 			</div>
 			<div className={s.board}>
 				{board.map(item => (
@@ -49,6 +49,7 @@ const BoardPage = () => {
 					</div>
 				))}
 			</div>
+			<div className={s.playerTwo}>{<PlayerBoard cards={Player2} onClickCard={card => setChoiceCard(card)} />}</div>
 		</div>
 	)
 }
